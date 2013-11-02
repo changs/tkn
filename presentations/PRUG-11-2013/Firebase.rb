5fd5c8b9585faf973e9ebefc45488a014a3136a2
Bartosz Pranczke <bartosz.pranczke@netguru.pl>
2013-11-02 14:58:25 +0100

    Add Firebase presentation

---
+++ presentations/PRUG-11-2013/Firebase.rb
@@ -0,0 +1,89 @@
+# encoding: utf-8
+# Introduction to Firebase
+
+center <<-AMEN
+  Introduction to Firebase
+  A scalable real-time backend
+
+  Bartosz Pranczke
+  PRUG 11-2013
+AMEN
+
+center <<-AMEN
+  How can we send a notification to already loaded page? 
+AMEN
+
+center <<-AMEN
+  Push technology
+AMEN
+
+center <<-AMEN
+  Long Pooling
+  Not really a push technology.
+AMEN
+
+center <<-AMEN
+  Pushlet
+AMEN
+
+center <<-AMEN
+  One serious drawback to this method is the lack of control the server has over the browser timing out;
+  a page refresh is always necessary if a timeout occurs on the browser end. 
+AMEN
+
+center <<-AMEN
+  Websockets 
+AMEN
+
+center <<-AMEN
+  Independent TCP-based protocol.
+AMEN
+
+center <<-AMEN
+  Works OK, but you need to take care of lots of details to achieve a sane real-time comunication. 
+AMEN
+
+center <<-AMEN
+  So, you want to create a real-time application, easy and without managing ulgy details of the backend?
+AMEN
+
+center <<-AMEN
+  Just kidding... backend is the most fun :)
+AMEN
+
+center <<-AMEN
+  Firebase 
+AMEN
+
+center <<-AMEN
+  Data stored in JSON. 
+AMEN
+
+center <<-AMEN
+  REST API to get the data. 
+AMEN
+
+image 'data_structure.png'
+
+center <<-AMEN
+  All data is kept locally, sync when online. 
+AMEN
+
+center <<-AMEN
+  Automatic Scaling  
+AMEN
+
+center <<-AMEN
+  Libraries for JS, Angular, Ember, Obj C, Java.
+AMEN
+
+center <<-AMEN
+  Sample app 
+AMEN
+
+center <<-AMEN
+  Thanks.
+  @bpranczke
+  changs @ github
+AMEN
+
